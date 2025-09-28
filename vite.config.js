438c9944e9e7b57876b506a5155d5aff047142c8
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-09-28 01:35:16 -0300

    Change base property

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dfe1_exp3_s1'
+  base: "https://nisiara.github.io/dfe1_exp3_s1/"
 })
